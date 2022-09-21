ce4ee8b97f0883a83683ff60d859dde2db44144d
FreeBird <2535759837@qq.com>
2022-09-21 22:19:51 +0800

    First

---
+++ First/cmake-build-debug/CMakeFiles/test_9_14.elf.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_9_14.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Core/Src/retarget.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/test_9_14.elf.dir/startup/startup_stm32f103xe.s.obj"
+  "test_9_14.elf"
+  "test_9_14.elf.manifest"
+  "test_9_14.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/test_9_14.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
